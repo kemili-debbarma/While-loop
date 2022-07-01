84fae269abb648ee22251815101511ee648ee915
kemili-debbarma <107979729+kemili-debbarma@users.noreply.github.com>
2022-07-01 11:29:07 +0530

    Add files via upload

---
+++ kml.py
@@ -0,0 +1,21 @@
+num=int(input("enter num"))
+if num>=1 and num<=5:
+  print(2*num)
+elif num>=6 and num<=10:
+  print(3*num)
+elif num>=10 and num<=15:
+  print(4*num)
+elif num>15:
+  print(5*num)
+else:
+  print("no calculat")
+
+
+
+
+
+
+
+
+
+  
